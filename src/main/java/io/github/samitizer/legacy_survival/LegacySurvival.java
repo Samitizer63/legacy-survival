3e3bfbf191083809579172f6227db1f18fc22840
Samitizer <samuelduddleston@gmail.com>
2023-12-15 22:41:15 -0600

    Initial commit

---
+++ src/main/java/io/github/samitizer/legacy_survival/LegacySurvival.java
@@ -0,0 +1,13 @@
+package io.github.samitizer.legacy_survival;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LegacySurvival implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
